210ad5ae05811fa1b51efa32d9644433a9f50a30
Kamin <kaminjfay@gmail.com>
2018-02-20 21:18:20 -0500

    Basic Setup

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Kamin Fay on 2/20/18.
+//
+
+#include <iostream>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+    std::cout << "Main function" << std::endl;
+
+    return EXIT_SUCCESS;
+}
