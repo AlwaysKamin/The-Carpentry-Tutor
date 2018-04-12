b851cf9c79e6ca97507b8a6e0934e4e5f8d81deb
Kamin <kaminjfay@gmail.com>
2018-04-12 03:39:37 -0400

    OpenCV for visual manipulation
    Included open cv. The camera opens a depth buffer and grabs that data but then goes and opens an rgb buffer, waits a few frames for auto balance to start and then grabs a frame. Converts the frame to an opencv mat where it can now be manipulated.

--- spike/distanceTest/distanceTest.cpp
+++ spike/distanceTest/distanceTest.cpp
@@ -3,8 +3,13 @@
 //
 #include <librealsense2/rs.hpp>
 #include <librealsense2/rsutil.h>
+#include <librealsense2/rs_advanced_mode.hpp>
 #include "example.hpp"
-
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+void getColorFrame(rs2::pipeline p, rs2::frameset frames);
+void getDepthFrame(rs2::pipeline p, rs2::frameset frames);
 
 int main(){
 
@@ -15,10 +20,23 @@ int main(){
     }
     rs2::device device = list.front();
 
+    rs2::config config;
+    config.enable_stream(RS2_STREAM_COLOR, 1920, 1080, RS2_FORMAT_BGR8, 6);
+    config.enable_stream(RS2_STREAM_DEPTH);
     rs2::pipeline p;
-    p.start();
+    p.start(config);
+    rs2::frameset frames;
+    rs2::frame colorFrame;
 
-    rs2::frameset frames = p.wait_for_frames();
+    getDepthFrame(p, frames);
+    getColorFrame(p, frames);
+
+    return EXIT_SUCCESS;
+}
+
+void getDepthFrame(rs2::pipeline p, rs2::frameset frames){
+
+    frames = p.wait_for_frames();
     rs2::depth_frame depth = frames.get_depth_frame();
 
     float cameraWidth = depth.get_width();
@@ -28,6 +46,20 @@ int main(){
 
     std::cout << "The object is " << dist_to_center << " Meteres away " << std::endl;
 
+}
 
-    return 0;
+void getColorFrame(rs2::pipeline p, rs2::frameset frames){
+    rs2::frame colorFrame;
+
+    for(int i = 0; i < 20; i++){
+        frames = p.wait_for_frames();
+        colorFrame = frames.get_color_frame();
+    }
+
+    cv::Mat colorImage(cv::Size(1920, 1080), CV_8UC3, (void*)colorFrame.get_data(), cv::Mat::AUTO_STEP);
+    //colorImage.data = (uchar*) reinterpret_cast<const uint8_t *>(colorFrame);
+    cv::cvtColor(colorImage, colorImage, CV_RGB2BGR);
+
+    cv::imshow("cameraData", colorImage);
+    cv::waitKey(-1);
 }
