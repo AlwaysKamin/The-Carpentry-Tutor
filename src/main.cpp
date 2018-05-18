d8efb4d02557569508d69e44f609f84e10644788
Kamin <kaminjfay@gmail.com>
2018-05-18 02:44:54 -0400

    Implementation of the windowing system in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,14 @@
 //
 
 #include <iostream>
-#include <stdlib.h>
+#include "window.h"
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[]) {
+
+    window testWindow("Test Window");
+
+    testWindow.render();
+
     std::cout << "Main function" << std::endl;
 
     return EXIT_SUCCESS;
