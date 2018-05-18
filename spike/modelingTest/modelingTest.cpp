927a874d80eb84980f7e004eb14a3a4298681755
Kamin <kaminjfay@gmail.com>
2018-05-18 02:45:49 -0400

    Modeling tests almost complete

--- spike/modelingTest/modelingTest.cpp
+++ spike/modelingTest/modelingTest.cpp
@@ -6,111 +6,118 @@
 #define SCREEN_HEIGHT  1080
 #define TOTAL_DEPTH 500
 
-struct centerPosition{
-    GLfloat x = SCREEN_WIDTH  / 2.0f;
+struct centerPosition {
+    GLfloat x = SCREEN_WIDTH / 2.0f;
     GLfloat y = SCREEN_HEIGHT / 2.0f;
-    GLfloat z =  -(TOTAL_DEPTH);
-}blockPos;
+    GLfloat z = -(TOTAL_DEPTH);
+} blockPos;
 
-struct dimensions{
+struct centerPositionFilled {
+    GLfloat x = SCREEN_WIDTH / 4.0f;
+    GLfloat y = SCREEN_HEIGHT / 4.0f;
+    GLfloat z = -(TOTAL_DEPTH);
+} blockPosFilled;
+
+struct dimensions {
     GLfloat depth;
     GLfloat length;
     GLfloat width;
-}block;
+} block;
 
-void keyCallback( GLFWwindow *window, int key, int scancode, int action, int mods );
+void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods);
+
 void renderBlock(centerPosition bPos, dimensions block);
+void renderBlockFilled(centerPositionFilled bPos, dimensions block);
 
 GLfloat rotationX = 0.0f;
 GLfloat rotationY = 0.0f;
 
-int main(){
+int main() {
     std::cout << "Input the blocks depth" << std::endl;
     std::cin >> block.depth;
-    std::cout <<"Input the blocks width" << std::endl;
+    std::cout << "Input the blocks width" << std::endl;
     std::cin >> block.width;
     std::cout << "input the blocks length" << std::endl;
     std::cin >> block.length;
 
     GLFWwindow *window;
-    if ( !glfwInit( ) )
-    {
+    if (!glfwInit()) {
         std::cerr << " There was an issue initializing GLFW" << std::endl;
         return -1;
     }
 
     //Window creation
-    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Model Testing", nullptr, nullptr );
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Model Testing", nullptr, nullptr);
 
-    glfwSetKeyCallback( window, keyCallback);
-    glfwSetInputMode( window, GLFW_STICKY_KEYS, 1 );
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, 1);
 
     int screenWidth = SCREEN_WIDTH;
     int screenHeight = SCREEN_HEIGHT;
 
-    glfwGetFramebufferSize( window, &screenWidth, &screenHeight);
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
 
-    if ( !window ){
-        glfwTerminate( );
+    if (!window) {
+        glfwTerminate();
         return -1;
     }
 
-    glfwMakeContextCurrent( window );
+    glfwMakeContextCurrent(window);
 
     // The section of the window openGL will display to
-    glViewport( 0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT );
+    glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     // Sets the matrix mode to projection which is used for camera manipulation; from what I can tell
-    glMatrixMode( GL_PROJECTION );
+    glMatrixMode(GL_PROJECTION);
     // Loads the matrix with the identity matrxi
-    glLoadIdentity( );
+    glLoadIdentity();
     // Sets up the camera info
     // Param 1: left clipping plane
     // Param 2: Right clipping plane
     // Param 3: Bottom clipping plane
     // Param 4; top clipping plane
     // Param 5 depth clipping plane
-    glOrtho( 0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1000 );
+    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1000);
 
-    glMatrixMode( GL_MODELVIEW ); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
-    glLoadIdentity( ); // same as above comment
+    glMatrixMode(
+            GL_MODELVIEW); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
+    glLoadIdentity(); // same as above comment
 
     // While window open
-    while (!glfwWindowShouldClose( window )){
+    while (!glfwWindowShouldClose(window)) {
         // Clears the buffer to the preset values
-        glClear( GL_COLOR_BUFFER_BIT );
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        glPushMatrix( );
-        glTranslatef( SCREEN_WIDTH / 2, SCREEN_HEIGHT/2, -500 );
-        glRotatef( rotationX, 1, 0, 0 );
-        glRotatef( rotationY, 0, 1, 0 );
-        glTranslatef( -(SCREEN_WIDTH / 2), -(SCREEN_HEIGHT/2) , 500 );
+        glPushMatrix();
+        glTranslatef(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, -500);
+        glRotatef(rotationX, 1, 0, 0);
+        glRotatef(rotationY, 0, 1, 0);
+        glTranslatef(-(SCREEN_WIDTH / 2), -(SCREEN_HEIGHT / 2), 500);
 
         //Calls Draw blocks to calculate and render the block
         //Param 1: block position struct
         // Param 2: Block dimensions struct
         renderBlock(blockPos, block);
+        //renderBlockFilled(blockPosFilled, block);
 
         glPopMatrix();
 
         // Swap front and back buffers
-        glfwSwapBuffers( window );
+        glfwSwapBuffers(window);
 
         //Events bullshit
-        glfwPollEvents( );
+        glfwPollEvents();
     }
     //Cleanup
-    glfwTerminate( );
+    glfwTerminate();
     return 0;
 }
 
 
-void keyCallback( GLFWwindow *window, int key, int scancode, int action, int mods ){
+void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods) {
     const GLfloat rotationSpeed = 20;
-    if ( action == GLFW_PRESS || action == GLFW_REPEAT )
-    {
-        switch ( key )
-        {
+    if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+        switch (key) {
             case GLFW_KEY_UP:
                 rotationX -= rotationSpeed;
                 break;
@@ -127,7 +134,7 @@ void keyCallback( GLFWwindow *window, int key, int scancode, int action, int mod
     }
 }
 
-void renderBlock(centerPosition bPos, dimensions block){
+void renderBlock(centerPosition bPos, dimensions block) {
     GLfloat vertices[] = {
             //Front face
             bPos.x - (0.5f * block.length), bPos.y + (0.5f * block.width), bPos.z + (0.5f * block.depth), // Top left
@@ -188,4 +195,71 @@ void renderBlock(centerPosition bPos, dimensions block){
 
     // Closes the client state
     glDisableClientState(GL_VERTEX_ARRAY);
+
+}
+
+void renderBlockFilled(centerPositionFilled bPos, dimensions block) {
+    GLfloat vertices[] = {
+            //Front face
+            bPos.x - (0.5f * block.length), bPos.y + (0.5f * block.width), bPos.z + (0.5f * block.depth), // Top left
+            bPos.x + (0.5f * block.length), bPos.y + (0.5f * block.width), bPos.z + (0.5f * block.depth), // Top Right
+            bPos.x + (0.5f * block.length), bPos.y - (0.5f * block.width), bPos.z + (0.5f * block.depth), // Bot Right
+            bPos.x - (0.5f * block.length), bPos.y - (0.5f * block.width), bPos.z + (0.5f * block.depth), // Bot Left
+
+            //Back face
+            bPos.x - (0.5f * block.length), bPos.y + (0.5f * block.width), bPos.z - (0.5f * block.depth), // Top left
+            bPos.x + (0.5f * block.length), bPos.y + (0.5f * block.width), bPos.z - (0.5f * block.depth), // Top Right
+            bPos.x + (0.5f * block.length), bPos.y - (0.5f * block.width), bPos.z - (0.5f * block.depth), // Bot Right
+            bPos.x - (0.5f * block.length), bPos.y - (0.5f * block.width), bPos.z - (0.5f * block.depth), // Bot Left
+
+            //Left face
+            bPos.x - (0.5f * block.length), bPos.y + (0.5f * block.width), bPos.z + (0.5f * block.depth), // Top left
+            bPos.x - (0.5f * block.length), bPos.y + (0.5f * block.width), bPos.z - (0.5f * block.depth), // Top Right
+            bPos.x - (0.5f * block.length), bPos.y - (0.5f * block.width), bPos.z - (0.5f * block.depth), // Bot Right
+            bPos.x - (0.5f * block.length), bPos.y - (0.5f * block.width), bPos.z + (0.5f * block.depth), // Bot Left
+
+            //Right face
+            bPos.x + (0.5f * block.length), bPos.y - (0.5f * block.width), bPos.z - (0.5f * block.depth), // Top left
+            bPos.x + (0.5f * block.length), bPos.y - (0.5f * block.width), bPos.z + (0.5f * block.depth), // Top Right
+            bPos.x + (0.5f * block.length), bPos.y + (0.5f * block.width), bPos.z + (0.5f * block.depth), // Bot Right
+            bPos.x + (0.5f * block.length), bPos.y + (0.5f * block.width), bPos.z - (0.5f * block.depth), // Bot Left
+
+            //Top face
+            bPos.x - (0.5f * block.length), bPos.y + (0.5f * block.width), bPos.z - (0.5f * block.depth), // Top left
+            bPos.x + (0.5f * block.length), bPos.y + (0.5f * block.width), bPos.z - (0.5f * block.depth), // Top Right
+            bPos.x + (0.5f * block.length), bPos.y + (0.5f * block.width), bPos.z + (0.5f * block.depth), // Bot Right
+            bPos.x - (0.5f * block.length), bPos.y + (0.5f * block.width), bPos.z + (0.5f * block.depth), // Bot Left
+
+            //back face
+            bPos.x - (0.5f * block.length), bPos.y - (0.5f * block.width), bPos.z - (0.5f * block.depth), // Top left
+            bPos.x + (0.5f * block.length), bPos.y - (0.5f * block.width), bPos.z - (0.5f * block.depth), // Top Right
+            bPos.x + (0.5f * block.length), bPos.y - (0.5f * block.width), bPos.z + (0.5f * block.depth), // Bot Right
+            bPos.x - (0.5f * block.length), bPos.y - (0.5f * block.width), bPos.z + (0.5f * block.depth), // Bot Left
+    };
+
+    // Front and Back is for front and back facing polygons
+    // GL_LINE is how the polygons are rastarized
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+    // Enables the vertex array for writing and rendering
+    glEnableClientState(GL_VERTEX_ARRAY);
+
+    //Defines an array of vertext data
+    // Param 1: 3 for the number of coordinates for each vertext (x, y, z)
+    // Param 2: GL_Float for the data type
+    // Param 3: 0 for no byte offset between consecutive vertices
+    // Param 4: the verticies themselves
+    glVertexPointer(3, GL_FLOAT, 0, vertices);
+
+    //Render primitives from array data
+    // Param 1: Mode
+    // Param 2: first index of array
+    // Param 3: number of indicies to render
+    glDrawArrays(GL_QUADS, 0, 24);
+
+    // Closes the client state
+    glDisableClientState(GL_VERTEX_ARRAY);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
 }
